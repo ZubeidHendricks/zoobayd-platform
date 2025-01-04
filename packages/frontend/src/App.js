5912486a7cd166ffcfe6e7e6b470831a1db3a454
ZubeidHendricks <zubeid.hendricks@gmail.com>
2025-01-04 19:35:48 +0200

    feat: add frontend base files

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement(
+      "header",
+      { className: "App-header" },
+      React.createElement("h1", null, "Welcome to Zoobayd Platform")
+    )
+  )
+}
+
+export default App
